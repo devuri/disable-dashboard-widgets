57a638437ddd0902a974c31554e240be972138ff
devuri <devuriko@gmail.com>
2020-12-17 14:58:03 -0500

    refactor: move logic to class

---
+++ src/class-dashboard-widgets.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace SwitchWeb;
+
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+if ( ! class_exists( 'SwitchWeb\Dashboard_Widgets' ) ) {
+
+	/**
+	 *  Handle Widget Removal
+	 *
+	 * TODO Add GUI to set who can see widgets ( subscriber, editor, contributer etc )
+	 *
+	 * @since 1.0
+	 */
+	final class Dashboard_Widgets {
+		/**
+		 * Dashboard_Widgets::initialize
+		 *
+		 * @return object
+		 */
+		public static function init() {
+			return new Dashboard_Widgets();
+		}
+
+		/**
+		 * Setup
+		 */
+		private function __construct() {
+			// init
+		}
+
+		/**
+		 * Disable Widgets
+		 *
+		 * @return void
+		 */
+		public function disable() {
+			add_action( 'wp_dashboard_setup', array( $this, 'widgets' ), 99 );
+		}
+
+		/**
+		 * Widgets
+		 *
+		 * @return void
+		 */
+		public function widgets() {
+			if ( ! current_user_can( 'manage_options' ) ) {
+			  global $wp_meta_boxes;
+			  $wp_meta_boxes['dashboard']['normal']['core'] = array();
+			  $wp_meta_boxes['dashboard']['side']['core'] = array();
+			}
+		}
+
+	}
+}
