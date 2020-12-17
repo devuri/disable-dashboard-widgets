83021de023e2f79672e90bcaaf5e3f034340e298
devuri <devuriko@gmail.com>
2020-12-17 14:59:53 -0500

    refactor: 0.1.4 Disable Dashboard Widgets

--- swdashboard-widgets-removal.php
+++ swdashboard-widgets-removal.php
@@ -1,67 +1,39 @@
 <?php
-/**
- * Plugin Name: Easy Dashboard Widgets Removal
- * Plugin URI:  http://switchwebdesign.io/wordpress-plugins/
- * Description: Easily Remove All Widgets from Dashboard for all users except the Administrator .
- * Author:      SwitchWebdesign.io
- * Author URI:  http://switchwebdesign.io
- * Version:     1.2.0
- * License:     GPLv2 or later
- * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain: swd-dashboard-widgets-removal
- * Domain Path: languages
- * Usage:
- * Tags:
+ /**
+ * Disable Dashboard Widgets
  *
- * Copyright 2020 Uriel Wilson	(email : uriel@switchwebdesign.io)
- * License: GNU General Public License
- * GPLv2 Full license details in license.txt
+ * @package   DisableDashboardWidgets
+ * @author    Uriel Wilson
+ * @copyright 2020 Uriel Wilson
+ * @license   GPL-2.0
+ * @link      https://urielwilson.com
  *
- * Easy Dashboard Widgets Removal  is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * Credit https://wordpress.stackexchange.com/questions/73561/how-to-remove-all-widgets-from-dashboard
- *
- * You should have received a copy of the GNU General Public License
- * along with Easy Dashboard Widgets Removal. If not, see http://www.gnu.org/licenses/
- * ----------------------------------------------------------------------------
- * @package   SwDashboardWidgetsRemoval
- * @author    Switch Web Dev
- * @link      https://switchwebdesign.io
- * @since     1.2.0
- * @license   GPL-2.0+
- * @copyright Copyright (c) 2020 Uriel Wilson.
- *  ----------------------------------------------------------------------------
+ * @wordpress-plugin
+ * Plugin Name:       Disable Dashboard Widgets
+ * Plugin URI:        https://switchwebdev.com/wordpress-plugins/
+ * Description:       This plugin will remove all widgets from Dashboard for all users except the Administrator.
+ * Version:           0.1.4
+ * Requires at least: 3.4
+ * Requires PHP:      5.6
+ * Author:            SwitchWebdev.com
+ * Author URI:        https://switchwebdev.com
+ * Text Domain:       disable-dashboard-widgets
+ * Domain Path:       languages
+ * License:           GPLv2
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  */
 
-  # deny direct access
+  	// deny direct access.
     if ( ! defined( 'WPINC' ) ) {
-      die;
+      	die;
     }
 
-  # plugin directory
-    define('SWDEDWR_DIR', dirname(__FILE__));
-
-  # plugin url
-    define('SWDEDWR_URL', plugins_url( '/',__FILE__ ));
-#  ----------------------------------------------------------------------------
+	/**
+	 *  Load class.
+	 */
+	require_once plugin_dir_path( __FILE__ ) . '/src/class-dashboard-widgets.php';
 
 	/**
-	 *  handle Widget Removal
-	 *
-	 * @since 1.0
+	 * Dashboard_Widgets  Handle Widget Removal
 	 */
-	add_action( 'wp_dashboard_setup', function () {
-		if ( ! current_user_can( 'manage_options' ) ) {
-		  global $wp_meta_boxes;
-		  $wp_meta_boxes['dashboard']['normal']['core'] = array();
-		  $wp_meta_boxes['dashboard']['side']['core'] = array();
-		}
-	}, 9999 );
+	SwitchWeb\Dashboard_Widgets::init()->disable();
